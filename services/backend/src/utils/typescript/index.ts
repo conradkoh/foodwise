9ce421f4730ab5d8890d36270e67e9eb1abed743
conradkoh <conradkoh@gmail.com>
2024-09-01 22:35:39 +0800

    impr: improve type utils for exact utility type

--- services/backend/src/utils/typescript/index.ts
+++ services/backend/src/utils/typescript/index.ts
@@ -1,1 +1,13 @@
+/**
+ * Removes undefined from a type
+ */
 export type RemoveUndefined<T> = T extends undefined ? never : T;
+
+/**
+ * Ensures that the type T is exactly the same as the type Shape with no extra properties
+ */
+export type Exact<T, Shape> = T extends Shape
+	? Exclude<keyof T, keyof Shape> extends never
+		? T
+		: never
+	: never;
