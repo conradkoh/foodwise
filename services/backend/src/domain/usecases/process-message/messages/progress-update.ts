00c0e7690867e8539f82d1c1ff0d05438a851cd6
conradkoh <conradkoh@gmail.com>
2024-09-05 23:58:32 +0800

    fix: deficit for summary progress update not using standard defict messaging

--- services/backend/src/domain/usecases/process-message/messages/progress-update.ts
+++ services/backend/src/domain/usecases/process-message/messages/progress-update.ts
@@ -1,9 +1,16 @@
+import { formatDeficitSurplus } from "@/domain/usecases/process-message/messages/fragments/deficit";
+
 export const PROGRESS_UPDATE_TEXT = (p: ProgressUpdateParams) => {
-	const deficit = p.caloriesOut - p.caloriesIn;
-	const deficitSymbol = deficit > 0 ? "👍🏼" : "‼️";
 	return `
 <b>Your progress for ${p.date} (${p.dayOfWeek})</b>
- - 🍔 In: ${Math.round(p.caloriesIn)} kcal, 🔥 Out: ${Math.round(p.caloriesOut)} kcal, ${deficitSymbol} Deficit: ${Math.round(deficit)} kcal
+ - 🍔 In: ${Math.round(p.caloriesIn)} kcal, 🔥 Out: ${Math.round(p.caloriesOut)} kcal, ${formatDeficitSurplus(
+		{
+			deficit: {
+				value: p.caloriesOut - p.caloriesIn,
+				units: "kcal",
+			},
+		},
+ )}
 `.trim();
 };
 
